497139772e9b2cc3239cbe8dc127263887941eb8
kevin <guancheng_@outlook.com>
2017-11-26 11:14:36 +0800

    function-v0.7

--- higher-order_function.js
+++ higher-order_function.js
@@ -6,6 +6,8 @@ function add ( x,y,f ) {
 
 console.log( add(-1,-10,Math.abs)); 
 
+//map的函数只能接受一个变量，对数组的每一个变量都执行这个操作,返回新数组,旧数组不发生改变！
+
 function pow( x ) {
     return x*x; 
 }
@@ -19,9 +21,10 @@ var add = function(x,y) {
 }
 var results2 = array1.reduce(add); 
 console.log( results2);     
+console.log( "map "+array1 ) ; 
 
 
-
+// reduce接受两个数组，递归式
 var connect = function( x,y ){
     return x*10+y; 
 }
@@ -43,3 +46,38 @@ var lowerOther = function( str ) {
 }; 
 
 console.log( lowerOther("asfAA")); 
+
+
+// filter 接受函数(只能返回true或者false)， x是当前元素，index是下标，self是数组变量
+var arr = ['a','b','c']; 
+var f = arr.filter( function(element,index,self){
+    console.log( self+" "+index+""+element ) ; 
+    return true; 
+} ); 
+var arr_duplicate = [10,20,30,10,20]; 
+var after_duplicate = arr_duplicate.filter( function( x ,index,self ) {
+    return index == self.indexOf( x ) ; 
+});
+console.log( after_duplicate ); 
+
+
+//sort函数会对当前数组排序！
+var sort_arr = [6,1,6,1,47,296,1,6,47,1,6,1]; 
+sort_arr.sort( function(x,y){
+    if ( x<y ) return -1 ; 
+    else return 1; 
+} ) ; 
+console.log(sort_arr); 
+
+function* fib(length) {
+    var a = 0 ; 
+    var b = 1 ; 
+    for (var i=0; i<length; i++ ) {
+        yield a; 
+        [a,b] = [b,a+b];     
+    }
+    return 1000; 
+}
+
+var obj; 
+var test = fib( 5 ) ; 
